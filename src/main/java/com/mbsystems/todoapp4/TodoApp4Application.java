4afa0b44b061191b80f6b239e57f45c803c1e1e0
basilkiwanuka <basilkiwanuka@icloud.com>
2023-02-07 08:11:51 -0500

    init

---
+++ src/main/java/com/mbsystems/todoapp4/TodoApp4Application.java
@@ -0,0 +1,13 @@
+package com.mbsystems.todoapp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApp4Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/mbsystems/todoapp4/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.mbsystems.todoapp4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(path = "/")
+    public String getIndex() {
+        return "index";
+    }
+}
